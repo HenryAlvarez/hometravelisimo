2a502a6e6de16c59be017d5a03210ca59bc66813
Henry Sunuc <henry.viaguate@gmail.com>
2020-05-08 17:56:55 -0600

    Updates

---
+++ precache-manifest.2041b5da75efab954900aca9229b0665.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f2bf7d21e79a92676b4c60bd69d69c",
+    "url": "/hometravelisimo/index.html"
+  },
+  {
+    "revision": "b5bf98a93e28f29ae7f8",
+    "url": "/hometravelisimo/static/css/2.35fcc229.chunk.css"
+  },
+  {
+    "revision": "d0a738ec9f4ebf6bcb08",
+    "url": "/hometravelisimo/static/css/main.ec636605.chunk.css"
+  },
+  {
+    "revision": "b5bf98a93e28f29ae7f8",
+    "url": "/hometravelisimo/static/js/2.51fdad0f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hometravelisimo/static/js/2.51fdad0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0a738ec9f4ebf6bcb08",
+    "url": "/hometravelisimo/static/js/main.6d0dbdad.chunk.js"
+  },
+  {
+    "revision": "6294ad013def34852de0",
+    "url": "/hometravelisimo/static/js/runtime-main.14d777fd.js"
+  },
+  {
+    "revision": "4cafa1ae80d27833de57434e43cbbe40",
+    "url": "/hometravelisimo/static/media/icomoon.4cafa1ae.ttf"
+  },
+  {
+    "revision": "59ce4e20d4977503b48f1a984ec28687",
+    "url": "/hometravelisimo/static/media/icomoon.59ce4e20.svg"
+  },
+  {
+    "revision": "9f24f49b58b1e5a440366cf91096ecbb",
+    "url": "/hometravelisimo/static/media/icomoon.9f24f49b.eot"
+  },
+  {
+    "revision": "a8f2a699d83d793922375f2a52eca883",
+    "url": "/hometravelisimo/static/media/icomoon.a8f2a699.woff"
+  },
+  {
+    "revision": "306646fe2b7a18436e6a8b3c7f1bea95",
+    "url": "/hometravelisimo/static/media/logotipo.306646fe.png"
+  },
+  {
+    "revision": "1372de09cfc3e9a62af4234fd331e8e9",
+    "url": "/hometravelisimo/static/media/poppins-v9-latin-500.1372de09.woff"
+  },
+  {
+    "revision": "597e1069f1687e85dda536d7eee9ea92",
+    "url": "/hometravelisimo/static/media/poppins-v9-latin-500.597e1069.eot"
+  },
+  {
+    "revision": "61e2d96d01a7eba5ea3ec1bad7e736a8",
+    "url": "/hometravelisimo/static/media/poppins-v9-latin-500.61e2d96d.woff2"
+  },
+  {
+    "revision": "6ceeb1499d325016fbe5fed4f971f722",
+    "url": "/hometravelisimo/static/media/poppins-v9-latin-500.6ceeb149.ttf"
+  },
+  {
+    "revision": "70e9051c6ebce0ffe149de16230903bf",
+    "url": "/hometravelisimo/static/media/poppins-v9-latin-500.70e9051c.svg"
+  },
+  {
+    "revision": "20cfc5c81c67573c98375b695e6a45c7",
+    "url": "/hometravelisimo/static/media/poppins-v9-latin-600.20cfc5c8.ttf"
+  },
+  {
+    "revision": "437844ee60c122bea4bea58e74389280",
+    "url": "/hometravelisimo/static/media/poppins-v9-latin-600.437844ee.svg"
+  },
+  {
+    "revision": "aa4405ed937295296cf8510f437628e0",
+    "url": "/hometravelisimo/static/media/poppins-v9-latin-600.aa4405ed.woff2"
+  },
+  {
+    "revision": "cf8e52f3dbc3bdaf571623e67df160a7",
+    "url": "/hometravelisimo/static/media/poppins-v9-latin-600.cf8e52f3.eot"
+  },
+  {
+    "revision": "e3bbab8d37ba508809f78c4baf02ebdb",
+    "url": "/hometravelisimo/static/media/poppins-v9-latin-600.e3bbab8d.woff"
+  },
+  {
+    "revision": "096b1b65599a30e8ef1dc280127e596c",
+    "url": "/hometravelisimo/static/media/rubik-v9-latin-500.096b1b65.eot"
+  },
+  {
+    "revision": "1978a872a96ba314807b126d7bbb0e26",
+    "url": "/hometravelisimo/static/media/rubik-v9-latin-500.1978a872.svg"
+  },
+  {
+    "revision": "1ea0be70443b012852c6c2379308c71c",
+    "url": "/hometravelisimo/static/media/rubik-v9-latin-500.1ea0be70.ttf"
+  },
+  {
+    "revision": "9ac3d9de00cce6d3352a889816226451",
+    "url": "/hometravelisimo/static/media/rubik-v9-latin-500.9ac3d9de.woff2"
+  },
+  {
+    "revision": "bec9243368f86e688b23bc1852823d0b",
+    "url": "/hometravelisimo/static/media/rubik-v9-latin-500.bec92433.woff"
+  },
+  {
+    "revision": "17bedde315941b70131ecf25e34e8f47",
+    "url": "/hometravelisimo/static/media/rubik-v9-latin-regular.17bedde3.ttf"
+  },
+  {
+    "revision": "3a0094a30ed24d998fc05c50cc36c6a8",
+    "url": "/hometravelisimo/static/media/rubik-v9-latin-regular.3a0094a3.svg"
+  },
+  {
+    "revision": "63c5b6176f60881d53dd2dc10904a04c",
+    "url": "/hometravelisimo/static/media/rubik-v9-latin-regular.63c5b617.woff2"
+  },
+  {
+    "revision": "80a9477d95ed5ca6e64843c1024e9e7f",
+    "url": "/hometravelisimo/static/media/rubik-v9-latin-regular.80a9477d.woff"
+  },
+  {
+    "revision": "abb19d44b5af117c2281ee62556a088d",
+    "url": "/hometravelisimo/static/media/rubik-v9-latin-regular.abb19d44.eot"
+  }
+]);
